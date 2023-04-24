ec01719f4cc72dd2df87067faceadc4d2e5e3bdc
unknown <observatorio@E211DIR01.etec211.sp.gov.br>
2023-04-24 14:20:26 -0300

    Segundo projeto com CHAT-GPT OPENAI

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
+const readlineSync = require("readline-sync");
+require("dotenv").config();
 
-const configuration = new Configuration({
+(async () => {
+  const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+  });
+  const openai = new OpenAIApi(configuration);
 
-async function runCompletion () {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Como você esta hoje?",
-    });
-    console.log(completion.data.choices[0].text);
-}
+  const history = [];
 
-runCompletion();
+  while (true) {
+    const user_input = readlineSync.question("Digite a sua pergunta: ");
+
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
+
+      history.push([user_input, completion_text]);
+
+      const user_input_again = readlineSync.question(
+        "\nVoce deseja continuar a nossa conversa? (Y/N)"
+      );
+      if (user_input_again.toUpperCase() === "N") {
+        return;
+      } else if (user_input_again.toUpperCase() !== "Y") {
+        console.log("Caracter invalido. Por favor pressione 'Y' ou 'N'.");
+        return;
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
+})();
